facde4a56ad6d713a633c21e557f40976245a431
Kiara Barias <barias.kiara@gmail.com>
2022-03-01 21:04:37 -0500

    github pages creation

---
+++ public/component---cache-caches-gatsby-plugin-offline-app-shell-js-85992f914a47332972df.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_solidstate=self.webpackChunkgatsby_starter_solidstate||[]).push([[306],{1367:function(t,e,a){var n=a(5318);e.__esModule=!0,e.default=void 0;var u=n(a(5354)),r=n(a(7294)),s=function(t){function e(){return t.apply(this,arguments)||this}return(0,u.default)(e,t),e.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},e}(r.default.Component),l=s;e.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-85992f914a47332972df.js.map
